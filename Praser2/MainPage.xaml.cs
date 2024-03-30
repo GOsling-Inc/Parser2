06e2a755766c37657a39fa779ce5ed4118013788
Lovely Paul <ppetrov228@bk.ru>
2024-03-30 21:07:36 +0300

    Parser Gilbo

---
+++ Praser2/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Services;
+using System.Text;
+
+namespace Praser2
+{
+    public partial class MainPage : ContentPage
+    {
+        Parser parser;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            parser = new Parser();
+            var template = new DataTemplate(() =>
+            {
+                var index = new Label { FontSize = 14, Margin = 10, HorizontalOptions = LayoutOptions.Center };
+                index.SetBinding(Label.TextProperty, new Binding { Path = "Index", StringFormat = "{0}" });
+
+                var name = new Label { FontSize = 14, Margin = 10, HorizontalOptions = LayoutOptions.Center };
+                name.SetBinding(Label.TextProperty, new Binding { Path = "Name", StringFormat = "{0}" });
+
+                var count = new Label { FontSize = 14, Margin = 10, HorizontalOptions = LayoutOptions.Center };
+                count.SetBinding(Label.TextProperty, new Binding { Path = "Count", StringFormat = "{0}" });
+
+                var grid = new Grid
+                {
+                    Padding = 5
+                };
+                grid.Add(index, 0, 0);
+                grid.Add(name, 1, 0);
+                grid.Add(count, 3, 0);
+                grid.SetColumnSpan(name, 2);
+                return grid;
+            });
+            List.ItemTemplate = template;
+        }
+
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            var file = await FilePicker.Default.PickAsync(new PickOptions
+            {
+                PickerTitle = "Pick source code"
+            });
+            if (file == null) return;
+            string text;
+            using (FileStream fstream = new(file.FullPath.ToString(), FileMode.Open))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                await fstream.ReadAsync(buffer);
+                text = Encoding.Default.GetString(buffer);
+            }
+            var results = parser.CountGilbeMetric(text);
+            List.ItemsSource = results.Item1;
+            operators.Text = $"Общее количество операторов: {results.Item2}";
+            cond_operators.Text = $"Количество условных операторов: {results.Item3}";
+            nesting.Text = $"Максимальный уровень вложенности условного оператора: {results.Item4}";
+            cl.Text = $"Насыщенность программы условными операторами: {results.Item5}";
+        }
+    }
+
+}
---
+++ Praser2/Services/Parser.cs
@@ -0,0 +1,178 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Services
+{
+    public class Item(string index, string name, string count)
+    {
+        public string Index { get; set; } = index;
+        public string Name { get; set; } = name;
+        public string Count { get; set; } = count;
+    }
+    public class Parser
+    {
+        readonly string[] allOperators = [";", "+", "-", "*", "/", "%", "**", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "=", "+=", "-=", "*=", "/=", "%=", "**=", "<<=", ">>=", "&=", "^=", "|=", "^", "<<", ">>", "~", ">>>"];
+        static Dictionary<string, int> operators = [];
+        public Parser() { }
+        public Tuple<List<Item>, int, int, int, float> CountGilbeMetric(string text)
+        {
+            int countConditions = CountAllCases(text) + CountAllIf(text) + CountAllLoops(text);
+            int allOperators = CountDefaultOperators(text) + countConditions;
+            int deepest = FindDeepestNesting(text);
+            var ops = new List<Item>
+            {
+                 new("индекс", "оператор", "количество")
+            };
+            var newoperators = from entry in operators orderby entry.Value descending select entry;
+            for (int i = 0; i < newoperators.Count(); i++)
+                if (newoperators.ElementAt(i).Value > 0)
+                    ops.Add(new Item((i + 1).ToString(), newoperators.ElementAt(i).Key, newoperators.ElementAt(i).Value.ToString()));
+            return Tuple.Create(ops, allOperators, countConditions, deepest, (float)countConditions / allOperators);
+        }
+
+        private static int CountCases(string text)
+        {
+            var caseRegex = new Regex(@"\b(case)\b");
+            var matches = caseRegex.Matches(text);
+            return matches.Count;
+        }
+
+        private static int CountAllCases(string text)
+        {
+            var matchRegex = new Regex(@"\bmatch\s*{((?:\s*(case)\s+[^=]+[^>+][=][>]\s*(?:(?<DEPTH>{)|(?<-DEPTH>})|[^{}]*)*(?(DEPTH)(?!)))+)");
+            int totalCases = 0;
+            operators["match"] = 0;
+            operators["case"] = 0;
+            foreach (Match match in matchRegex.Matches(text))
+            {
+                operators["match"]++;
+                string body = match.Groups[1].Value.Trim();
+                totalCases += CountCases(body) - 1;
+            }
+            operators["case"] = totalCases + 1;
+            return totalCases;
+        }
+
+        private static int CountAllIf(string text)
+        {
+            var ifRegex = new Regex(@"(\bif\b|\}\s*else if|\belse\b)");
+            var matches = ifRegex.Matches(text);
+            operators["if"] = operators["else if"] = operators["else"] = 0;
+            foreach(Match match in matches)
+            {
+                if (match.ToString() == "if" || match.ToString() == "else")
+                    operators[match.ToString()]++;
+                else operators["else if"]++;
+            }
+            return matches.Count;
+        }
+
+        private static int CountAllLoops(string text)
+        {
+            var loopRegex = new Regex(@"\b(for|while)\b");
+            var matches = loopRegex.Matches(text);
+            operators["for"] = operators["while"] = 0;
+            foreach(Match match in matches)
+            {
+                operators[match.ToString()]++;
+            }
+            return matches.Count;
+        }
+
+        private int CountDefaultOperators(string code)
+        {
+            int count = 0;
+            foreach (var item in allOperators)
+            {
+                var newItem = "";
+                foreach (var i in item)
+                {
+                    newItem += $@"\{i}";
+                }
+                Regex operatorsReg = new($"[0-9a-zA-Z\r\n ]({newItem})[0-9a-zA-Z\r\n ]");
+                var op_matches = operatorsReg.Matches(code);
+                count += op_matches.Count;
+                if ( op_matches.Count > 0 )
+                    operators[item] = op_matches.Count;
+            }
+            Regex point = new(@"[a-zA-Z]+[a-zA-Z0-9_]*(\.)[a-zA-Z]+[a-zA-Z0-9_]*");
+            var matches = point.Matches(code);
+            count += matches.Count;
+            if ( matches.Count > 0 )
+                operators["."] = matches.Count;
+            return count;
+        }
+
+        private static int DeepestMatchStatements(string text)
+        {
+            var matchRegex = new Regex(@"\bmatch\s*{((?:\s*(case)\s+[^=]+[^>+][=][>]\s*(?:(?<DEPTH>{)|(?<-DEPTH>})|[^{}]*)*(?(DEPTH)(?!)))+)");
+            int maxCountedCases = 0;
+            foreach (Match match in matchRegex.Matches(text))
+            {
+                string body = match.Groups[2].Value.Trim();
+                maxCountedCases = Math.Max(CountMatchNesting(body, maxCountedCases), maxCountedCases);
+            }
+            return maxCountedCases;
+        }
+
+        private static int CountMatchNesting(string text, int currentNestingLevel)
+        {
+            int maxNestingLevel = currentNestingLevel;
+            var caseRegex = new Regex(@"\bcase\s+[^=]+[^>+][=][>]\s*(\{*\s*.*\s*\}*)");
+            var matches = caseRegex.Matches(text);
+            foreach (Match match in matches)
+            {
+                string caseBody = match.Groups[1].Value.Trim();
+                maxNestingLevel = Math.Max(maxNestingLevel, CountNestingLevel(caseBody, currentNestingLevel + 1));
+            }
+            return maxNestingLevel;
+        }
+
+        private static int CountNestingLevel(string body, int currentNestingLevel)
+        {
+            int maxNestingLevel = currentNestingLevel;
+
+            var nestedRegex = new Regex(@"\b(else if|if|match|else|for|while)\s*\(*(.*?)\)*\s*{((?:{(?<DEPTH>)|(?<-DEPTH>})|(?>[^{}]+))*(?(DEPTH)(?!)))\}");
+            var nestedMatches = nestedRegex.Matches(body);
+
+
+            foreach (Match nestedMatch in nestedMatches)
+            {
+                string operatorType = nestedMatch.Groups[1].Value;
+                string nestedBody = nestedMatch.Groups[3].Value.Trim();
+
+                int nestingLevel = CountNestingLevel(nestedBody, currentNestingLevel + 1);
+                maxNestingLevel = Math.Max(maxNestingLevel, nestingLevel);
+            }
+
+            if (body.Contains("match"))
+            {
+                int switchDepth = CountCases(body) + DeepestMatchStatements(body) - 2 + currentNestingLevel;
+                maxNestingLevel = Math.Max(maxNestingLevel, switchDepth);
+            }
+
+            return maxNestingLevel;
+        }
+
+        private static int FindDeepestNesting(string text)
+        {
+            var regex = new Regex(@"\b(else if|if|match|else|for|while)\s*\(*(.*?)\)*\s*{((?:{(?<DEPTH>)|(?<-DEPTH>})|(?>[^{}]+))*(?(DEPTH)(?!)))\}");
+            var matches = regex.Matches(text);
+            int maxNestingLevel = 0;
+            foreach (Match match in matches)
+            {
+                string operatorType = match.Groups[1].Value;
+                string body = match.Groups[3].Value.Trim();
+                int nestingLevel = 0;
+                if (operatorType == "match")
+                    nestingLevel = CountMatchNesting(body, CountCases(body) - 2);
+                else
+                    nestingLevel = CountNestingLevel(body, 0);
+
+                maxNestingLevel = Math.Max(maxNestingLevel, nestingLevel);
+            }
+
+            return maxNestingLevel;
+        }
+
+    }
+}
